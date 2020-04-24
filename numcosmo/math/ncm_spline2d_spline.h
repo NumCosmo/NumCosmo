19851f276ae7a4766e556653e965d6ead17c70d6
Fernando de Simoni <fernando.saliby@gmail.com>
2020-04-24 16:58:46 -0300

    Fixed indentation:
       * ncm_spline2d_spline.h/c
       * ncm_spline2d_gsl.h/c

--- numcosmo/math/ncm_spline2d_spline.h
+++ numcosmo/math/ncm_spline2d_spline.h
@@ -5,6 +5,7 @@
  *  Copyright  2010  Mariana Penna Lima & Sandro Dias Pinto Vitenti
  *  <pennalima@gmail.com>, <sandro@isoftware.com.br>
  ****************************************************************************/
+
 /*
  * numcosmo
  * Copyright (C) Mariana Penna Lima & Sandro Dias Pinto Vitenti 2012 <pennalima@gmail.com>, <sandro@isoftware.com.br>
@@ -12,12 +13,12 @@
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * numcosmo is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -74,3 +75,4 @@ NcmSpline2d *ncm_spline2d_spline_new (NcmSpline *s);
 G_END_DECLS
 
 #endif /* _NCM_SPLINE2D_SPLINE_H_ */
+
