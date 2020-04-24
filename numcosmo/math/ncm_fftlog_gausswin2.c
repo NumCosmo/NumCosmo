c3e21160cc93a39001a2f5fde7078a0c8ce644c2
Fernando de Simoni <fernando.saliby@gmail.com>
2020-04-24 13:47:37 -0300

    Fixed indentation:
       * ncm_fftlog_tophatwin2.h/c
       * ncm_fftlog_gausswin2.h/c

--- numcosmo/math/ncm_fftlog_gausswin2.c
+++ numcosmo/math/ncm_fftlog_gausswin2.c
@@ -6,6 +6,7 @@
  *  <sandro@isoftware.com.br>
  ****************************************************************************/
 /* excerpt from: */
+
 /***************************************************************************
  *            nc_window_gaussian.c
  *
@@ -13,6 +14,7 @@
  *  Copyright  2010  Mariana Penna Lima
  *  <pennalima@gmail.com>
  ****************************************************************************/
+
 /*
  * ncm_fftlog_gausswin2.c
  * Copyright (C) 2014 Sandro Dias Pinto Vitenti <sandro@isoftware.com.br>
@@ -21,12 +23,12 @@
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * numcosmo is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -45,7 +47,7 @@
  * \begin{equation}
  * W(t) = \exp \left( \frac{-t^2}{2} \right).
  * \end{equation}
- *  
+ *
  */
 
 #ifdef HAVE_CONFIG_H
@@ -72,7 +74,6 @@ ncm_fftlog_gausswin2_init (NcmFftlogGausswin2 *gwin2)
 static void
 _ncm_fftlog_gausswin2_finalize (GObject *object)
 {
-
   /* Chain up : end */
   G_OBJECT_CLASS (ncm_fftlog_gausswin2_parent_class)->finalize (object);
 }
@@ -82,38 +83,40 @@ static void _ncm_fftlog_gausswin2_get_Ym (NcmFftlog *fftlog, gpointer Ym_0);
 static void
 ncm_fftlog_gausswin2_class_init (NcmFftlogGausswin2Class *klass)
 {
-  GObjectClass* object_class   = G_OBJECT_CLASS (klass);
+  GObjectClass *object_class   = G_OBJECT_CLASS (klass);
   NcmFftlogClass *fftlog_class = NCM_FFTLOG_CLASS (klass);
-
+  
   object_class->finalize = &_ncm_fftlog_gausswin2_finalize;
-
-  fftlog_class->name        = "gaussian_window_2";
-  fftlog_class->get_Ym      = &_ncm_fftlog_gausswin2_get_Ym;
+  
+  fftlog_class->name   = "gaussian_window_2";
+  fftlog_class->get_Ym = &_ncm_fftlog_gausswin2_get_Ym;
 }
 
-static void 
+static void
 _ncm_fftlog_gausswin2_get_Ym (NcmFftlog *fftlog, gpointer Ym_0)
 {
-  const gdouble twopi_Lt  = 2.0 * M_PI / ncm_fftlog_get_full_length (fftlog);
-  const gint Nf           = ncm_fftlog_get_full_size (fftlog);
+  const gdouble twopi_Lt = 2.0 * M_PI / ncm_fftlog_get_full_length (fftlog);
+  const gint Nf          = ncm_fftlog_get_full_size (fftlog);
+  
 #ifdef NUMCOSMO_HAVE_FFTW3
   fftw_complex *Ym_base = (fftw_complex *) Ym_0;
   gint i;
-
+  
   for (i = 0; i < Nf; i++)
   {
     const gint phys_i            = ncm_fftlog_get_mode_index (fftlog, i);
     const complex double a       = twopi_Lt * phys_i * I;
-    const complex double A       = a + 0.0/*fftlog->nu*/;
+    const complex double A       = a + 0.0 /*fftlog->nu*/;
     const complex double onepA_2 = 0.5 * (1.0 + A);
     complex double U;
     gsl_sf_result lngamma_rho, lngamma_theta;
-
+    
     gsl_sf_lngamma_complex_e (creal (onepA_2), cimag (onepA_2), &lngamma_rho, &lngamma_theta);
     U = 0.5 * cexp (lngamma_rho.val + I * lngamma_theta.val);
-
+    
     Ym_base[i] = U;
   }
+  
 #endif /* NUMCOSMO_HAVE_FFTW3 */
 }
 
@@ -123,19 +126,21 @@ _ncm_fftlog_gausswin2_get_Ym (NcmFftlog *fftlog, gpointer Ym_0)
  * @lnk0: input center $\ln(k_0)$
  * @Lk: input/output interval size
  * @N: number of knots
- * 
+ *
  * Creates a new fftlog Gaussian window squared object.
- * 
+ *
  * Returns: (transfer full): a new #NcmFftlogGausswin2
  */
 NcmFftlogGausswin2 *
 ncm_fftlog_gausswin2_new (gdouble lnr0, gdouble lnk0, gdouble Lk, guint N)
 {
-  NcmFftlogGausswin2 *fftlog = g_object_new (NCM_TYPE_FFTLOG_GAUSSWIN2, 
+  NcmFftlogGausswin2 *fftlog = g_object_new (NCM_TYPE_FFTLOG_GAUSSWIN2,
                                              "lnr0", lnr0,
                                              "lnk0", lnk0,
                                              "Lk", Lk,
                                              "N", N,
                                              NULL);
+  
   return fftlog;
 }
+
