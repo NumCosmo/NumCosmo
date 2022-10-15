30537e7a8b1935f186f402f27eb39489904ef044
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2022-10-15 12:12:20 -0300

    Updated autotools. Added numcosmo-valgrind.supp for valgrind memcheck. Fixed optimization bug in ncm_sphere_map.

--- numcosmo/math/ncm_sphere_map.h
+++ numcosmo/math/ncm_sphere_map.h
@@ -14,12 +14,12 @@
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * numcosmo is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -61,16 +61,16 @@ struct _NcmSphereMapClass
  * NcmSphereMapOrder:
  * @NCM_SPHERE_MAP_ORDER_NEST: FIXME
  * @NCM_SPHERE_MAP_ORDER_RING: FIXME
- * 
+ *
  * FIXME
- * 
+ *
  */
 typedef enum _NcmSphereMapOrder
 {
   NCM_SPHERE_MAP_ORDER_NEST,
-  NCM_SPHERE_MAP_ORDER_RING, 
+  NCM_SPHERE_MAP_ORDER_RING,
   /* < private > */
-  NCM_SPHERE_MAP_ORDER_LEN,  /*< skip >*/
+  NCM_SPHERE_MAP_ORDER_LEN, /*< skip >*/
 } NcmSphereMapOrder;
 
 /**
@@ -78,15 +78,15 @@ typedef enum _NcmSphereMapOrder
  * @NCM_SPHERE_MAP_COORD_SYS_GALACTIC: FIXME
  * @NCM_SPHERE_MAP_COORD_SYS_ECLIPTIC: FIXME
  * @NCM_SPHERE_MAP_COORD_SYS_CELESTIAL: FIXME
- * 
+ *
  * FIXME
- * 
+ *
  */
 typedef enum _NcmSphereMapCoordSys
 {
-  NCM_SPHERE_MAP_COORD_SYS_GALACTIC  = 'G',
-  NCM_SPHERE_MAP_COORD_SYS_ECLIPTIC  = 'E',
-  NCM_SPHERE_MAP_COORD_SYS_CELESTIAL = 'C', 
+  NCM_SPHERE_MAP_COORD_SYS_GALACTIC = 'G',
+  NCM_SPHERE_MAP_COORD_SYS_ECLIPTIC = 'E',
+  NCM_SPHERE_MAP_COORD_SYS_CELESTIAL = 'C',
   /* < private > */
   NCM_SPHERE_MAP_COORD_SYS_LEN, /*< skip >*/
 } NcmSphereMapCoordSys;
@@ -167,38 +167,39 @@ void ncm_sphere_map_alm2map (NcmSphereMap *smap);
 NcmSpline *ncm_sphere_map_calc_Ctheta (NcmSphereMap *smap, const gdouble reltol);
 
 #define NCM_SPHERE_MAP_N(nside) (12 * (nside) * (nside))
-#define NCM_SPHERE_MAP_INT_TO_XY(i,x,y) \
-G_STMT_START { \
-  gint shift = 0, shifted = i; \
-  x = y = 0; \
-  do { \
-    x |= ((shifted & 1) << shift); \
-    y |= (((shifted & 2) >> 1) << shift); \
-    shift++; \
-  } while (shifted >>= 2); \
-} G_STMT_END
+#define NCM_SPHERE_MAP_INT_TO_XY(i, x, y) \
+  G_STMT_START { \
+    gint shift = 0, shifted = i; \
+    x          = y = 0; \
+    do { \
+      x |= ((shifted & 1) << shift); \
+      y |= (((shifted & 2) >> 1) << shift); \
+      shift++; \
+    } while (shifted >>= 2); \
+  } G_STMT_END
 
-#define NCM_SPHERE_MAP_XY_TO_INT(x,y,i) \
-G_STMT_START { \
-  gint shift = 0, shifted_x = x, shifted_y = y; \
-  g_assert (shifted_x >= 0 && shifted_y >= 0); \
-  i = 0; \
-  do { \
-    i |= ((shifted_x & 1) << (shift + 0)); \
-    i |= ((shifted_y & 1) << (shift + 1)); \
-    shift += 2; shifted_x >>= 1; shifted_y >>= 1; \
-  } while (shifted_x || shifted_y); \
-} G_STMT_END
+#define NCM_SPHERE_MAP_XY_TO_INT(x, y, i) \
+  G_STMT_START { \
+    gint shift = 0, shifted_x = x, shifted_y = y; \
+    g_assert (shifted_x >= 0 && shifted_y >= 0); \
+    i = 0; \
+    do { \
+      i     |= ((shifted_x & 1) << (shift + 0)); \
+      i     |= ((shifted_y & 1) << (shift + 1)); \
+      shift += 2; shifted_x >>= 1; shifted_y >>= 1; \
+    } while (shifted_x || shifted_y); \
+  } G_STMT_END
 
 #define NCM_SPHERE_MAP_HEALPIX_NULLVAL (-1.6375e30)
 #define NCM_SPHERE_MAP_DEFAULT_SIGNAL "SIGNAL"
 
-#define NCM_SPHERE_MAP_ALM_SIZE(lmax) (((lmax)*(lmax) + 3*(lmax) + 2)/2) 
-#define NCM_SPHERE_MAP_M_START(lmax,m) ((2*(lmax)*(m)-(m)*(m)+3*(m))/2)
+#define NCM_SPHERE_MAP_ALM_SIZE(lmax) (((lmax) * (lmax) + 3 * (lmax) + 2) / 2)
+#define NCM_SPHERE_MAP_M_START(lmax, m) ((2 * (lmax) * (m) - (m) * (m) + 3 * (m)) / 2)
 
 /*#define NCM_SPHERE_MAP_ALM_INDEX(l,m) ((l * (l + 1)) / 2 + m)*/
-#define NCM_SPHERE_MAP_ALM_INDEX(lmax,l,m) ((l) + ((1 + 2 * (lmax) - (m)) * (m)) / 2)
+#define NCM_SPHERE_MAP_ALM_INDEX(lmax, l, m) ((l) + ((1 + 2 * (lmax) - (m)) * (m)) / 2)
 
 G_END_DECLS
 
 #endif /* _NCM_SPHERE_MAP_H_ */
+
