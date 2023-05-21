3a34b9ebc568b63323b9022c64adb5355381e708
Caio Oliveira <caiolimadeoliveira@proton.me>
2023-05-20 23:08:07 -0300

    New nc_galaxy_sd_z_proxy object prototype

---
+++ numcosmo/galaxy/nc_galaxy_sd_z_proxy.c
@@ -0,0 +1,163 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 2; tab-width: 2 -*-  */
+
+/***************************************************************************
+ *            nc_galaxy_sd_z_proxy.c
+ *
+ *  Sat May 20 17:52:48 2023
+ *  Copyright  2023  Caio Lima de Oliveira
+ *  <caiolimadeoliveira@pm.me>
+ ****************************************************************************/
+/*
+ * nc_galaxy_sd_z_proxy.c
+ * Copyright (C) 2023 Caio Lima de Oliveira <caiolimadeoliveira@pm.me>
+ *
+ * numcosmo is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * numcosmo is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SECTION: nc_galaxy_sd_z_proxy
+ * @title: NcGalaxySDZProxy
+ * @short_description: Class describing galaxy sample position distributions.
+ * @stability: Unstable
+ *
+ *
+ * This class describes a galaxy sample position distributions.
+ * It is composed by two distributions: an imagem position distribution $P(r)$ and a redshift distribution $P(z)$.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif /* HAVE_CONFIG_H */
+#include "build_cfg.h"
+
+#include "galaxy/nc_galaxy_sd_z_proxy.h"
+#include <math.h>
+#include <gsl/gsl_math.h>
+
+
+struct _NcGalaxySDZProxyPrivate
+{
+  gint placeholder;
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE(NcGalaxySDZProxy, nc_galaxy_sd_z_proxy, G_TYPE_OBJECT);
+
+static void
+nc_galaxy_sd_z_proxy_init (NcGalaxySDZProxy *gsdzp)
+{
+  gsdzp->priv = nc_galaxy_sd_z_proxy_get_instance_private (gsdzp);
+}
+
+static void
+_nc_galaxy_sd_z_proxy_finalize (GObject *object)
+{
+  G_OBJECT_CLASS (nc_galaxy_sd_z_proxy_parent_class)->finalize (object);
+}
+
+static gdouble
+_nc_galaxy_sd_z_proxy_gen (NcGalaxySDZProxy *gsdzp, NcHICosmo *cosmo, NcHaloDensityProfile *dp, NcWLSurfaceMassDensity *smd, const gdouble z_cluster, NcmRNG *rng, const gdouble zp)
+{
+  g_error ("_nc_galaxy_sd_z_proxy_gen: method not implemented.");
+
+  return 0.0;
+}
+
+static gdouble
+_nc_galaxy_sd_z_proxy_integ (NcGalaxySDZProxy *gsdzp, NcHICosmo *cosmo, NcHaloDensityProfile *dp, NcWLSurfaceMassDensity *smd, const gdouble z_cluster, const gdouble zp)
+{
+  g_error ("_nc_galaxy_sd_z_proxy_integ: method not implemented.");
+
+  return 0.0;
+}
+
+static void
+nc_galaxy_sd_z_proxy_class_init (NcGalaxySDZProxyClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->finalize = &_nc_galaxy_sd_z_proxy_finalize;
+
+  klass->gen   = &_nc_galaxy_sd_z_proxy_gen;
+  klass->integ = &_nc_galaxy_sd_z_proxy_integ;
+}
+
+
+/**
+ * nc_galaxy_sd_z_proxy_ref:
+ * @gsdzp: a #NcGalaxySDZProxy
+ *
+ * Increase the reference of @gsdzp by one.
+ *
+ * Returns: (transfer full): @gsdzp.
+ */
+NcGalaxySDZProxy *
+nc_galaxy_sd_z_proxy_ref (NcGalaxySDZProxy *gsdzp)
+{
+  return g_object_ref (gsdzp);
+}
+
+/**
+ * nc_galaxy_sd_z_proxy_free:
+ * @gsdzp: a #NcGalaxySDZProxy
+ *
+ * Decrease the reference count of @gsdzp by one.
+ *
+ */
+void
+nc_galaxy_sd_z_proxy_free (NcGalaxySDZProxy *gsdzp)
+{
+  g_object_unref (gsdzp);
+}
+
+/**
+ * nc_galaxy_sd_z_proxy_clear:
+ * @gsdzp: a #NcGalaxySDZProxy
+ *
+ * Decrease the reference count of @gsdzp by one, and sets the pointer *@gsdzp to
+ * NULL.
+ *
+ */
+void
+nc_galaxy_sd_z_proxy_clear (NcGalaxySDZProxy **gsdzp)
+{
+  g_clear_object (gsdzp);
+}
+
+/**
+ * nc_galaxy_sd_z_proxy_gen: (virtual gen)
+ * @gwld: a #NcGalaxyWLDist
+ * @cosmo: a #NcHICosmo
+ * @dp: a #NcHaloDensityProfile
+ * @smd: a #NcWLSurfaceMassDensity
+ * @z_cluster: cluster redshift $z_\mathrm{cl}
+ * @rng: a #NcmRNG
+ *
+ * Generates a $z_p$ value from the distribution using @rng.
+ *
+ * Returns: the generated value $z_p$.
+ */
+/**
+ * nc_galaxy_sd_z_proxy_integ: (virtual integ)
+ * @gwld: a #NcGalaxyWLDist
+ * @cosmo: a #NcHICosmo
+ * @dp: a #NcHaloDensityProfile
+ * @smd: a #NcWLSurfaceMassDensity
+ * @z_cluster: cluster redshift $z_\mathrm{cl}$
+ *
+ * Computes the probability density of the observable $z_p$ given the redshift.
+ * The probability density is given by $P(z_p)P$.
+ *
+ * Returns: the probability density at $z_p$, $P(z_p)$.
+ */
---
+++ numcosmo/galaxy/nc_galaxy_sd_z_proxy.h
@@ -0,0 +1,106 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 2; tab-width: 2 -*-  */
+
+/***************************************************************************
+ *            nc_galaxy_sd_z_proxy.h
+ *
+ *  Sat May 20 18:41:47 2023
+ *  Copyright  2023  Caio Lima de Oliveira
+ *  <caiolimadeoliveira@pm.me>
+ ****************************************************************************/
+/*
+ * nc_galaxy_sd_z_proxy.h
+ * Copyright (C) 2023 Caio Lima de Oliveira <caiolimadeoliveira@pm.me>
+ *
+ * numcosmo is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * numcosmo is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _NC_GALAXY_SD_Z_PROXY_H_
+#define _NC_GALAXY_SD_Z_PROXY_H_
+
+#include <glib.h>
+#include <glib-object.h>
+#include <numcosmo/build_cfg.h>
+
+
+G_BEGIN_DECLS
+
+#define NC_TYPE_GALAXY_SD_POSITION             (nc_galaxy_sd_z_proxy_get_type ())
+#define NC_GALAXY_SD_Z_PROXY(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), NC_TYPE_GALAXY_SD_POSITION, NcGalaxySDZProxy))
+#define NC_GALAXY_SD_Z_PROXY_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), NC_TYPE_GALAXY_SD_POSITION, NcGalaxySDZProxyClass))
+#define NC_IS_GALAXY_SD_POSITION(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NC_TYPE_GALAXY_SD_POSITION))
+#define NC_IS_GALAXY_SD_POSITION_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), NC_TYPE_GALAXY_SD_POSITION))
+#define NC_GALAXY_SD_Z_PROXY_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), NC_TYPE_GALAXY_SD_POSITION, NcGalaxySDZProxyClass))
+
+typedef struct _NcGalaxySDZProxyClass NcGalaxySDZProxyClass;
+typedef struct _NcGalaxySDZProxy NcGalaxySDZProxy;
+typedef struct _NcGalaxySDZProxyPrivate NcGalaxySDZProxyPrivate;
+
+struct _NcGalaxySDZProxyClass
+{
+  /*< private >*/
+  NcGalaxySDZProxyDistClass parent_class;
+};
+
+struct _NcGalaxySDZProxy
+{
+  /*< private >*/
+  NcGalaxySDZProxyDist parent_instance;
+  NcGalaxySDZProxyPrivate *priv;
+};
+
+GType nc_galaxy_sd_z_proxy_get_type (void) G_GNUC_CONST;
+
+NcGalaxySDZProxy *nc_galaxy_sd_z_proxy_new (NcGalaxySDShape *s_dist, NcGalaxySDZProxy *zp_dist, NcGalaxySDZProxy *rz_dist);
+NcGalaxySDZProxy *nc_galaxy_sd_z_proxy_ref (NcGalaxySDZProxy *gsdzp);
+
+void nc_galaxy_sd_z_proxy_free (NcGalaxySDZProxy *gsdzp);
+void nc_galaxy_sd_z_proxy_clear (NcGalaxySDZProxy **gsdzp);
+
+NCM_INLINE gdouble nc_galaxy_sd_z_proxy_gen (NcGalaxySDZProxy *gsdzp, NcHICosmo *cosmo, NcHaloDensityProfile *dp, NcWLSurfaceMassDensity *smd, const gdouble z_cluster, NcmRNG *rng, const gdouble zp);
+NCM_INLINE gdouble nc_galaxy_sd_z_proxy_integ (NcGalaxySDZProxy *gsdzp, NcHICosmo *cosmo, NcHaloDensityProfile *dp, NcWLSurfaceMassDensity *smd, const gdouble z_cluster, const gdouble zp);
+
+G_END_DECLS
+
+#endif /* _NC_GALAXY_SD_Z_PROXY_H_ */
+
+#ifndef _NC_GALAXY_SD_Z_PROXY_INLINE_H_
+#define _NC_GALAXY_SD_Z_PROXY_INLINE_H_
+#ifdef NUMCOSMO_HAVE_INLINE
+#ifndef __GTK_DOC_IGNORE__
+
+G_BEGIN_DECLS
+
+NCM_INLINE void
+nc_galaxy_sd_z_proxy_gen (NcGalaxySDZProxy *gsdzp, NcHICosmo *cosmo, NcHaloDensityProfile *dp, NcWLSurfaceMassDensity *smd, const gdouble z_cluster, NcmRNG *rng, const gdouble zp)
+{
+  NcGalaxySDZProxyClass *gsdzp_class = NC_GALAXY_SD_Z_PROXY_GET_CLASS (gsdzp);
+
+  if (gsdzp_class->gen != NULL)
+    NC_GALAXY_SD_Z_PROXY_GET_CLASS (gsdzp)->gen (gsdzp, cosmo, dp, smd, z_cluster, rng);
+}
+
+NCM_INLINE void
+nc_galaxy_sd_z_proxy_integ (NcGalaxySDZProxy *gsdzp, NcHICosmo *cosmo, NcHaloDensityProfile *dp, NcWLSurfaceMassDensity *smd, const gdouble z_cluster, const gdouble zp)
+{
+  NcGalaxySDZProxyClass *gsdzp_class = NC_GALAXY_SD_Z_PROXY_GET_CLASS (gsdzp);
+
+  if (gsdzp_class->integ != NULL)
+    NC_GALAXY_SD_Z_PROXY_GET_CLASS (gsdzp)->integ (gsdzp, cosmo, dp, smd, z_cluster);
+}
+
+G_END_DECLS
+
+#endif /* __GTK_DOC_IGNORE__ */
+#endif /* NUMCOSMO_HAVE_INLINE */
+#endif /* _NC_GALAXY_SD_Z_PROXY_INLINE_H_ */
+
