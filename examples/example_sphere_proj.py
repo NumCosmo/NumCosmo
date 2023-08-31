7d73c476c8149139cd03d0a915f15c060bb7f59a
henrique.cnl@hotmail.com <henrique.cnl@hotmail.com>
2023-08-31 12:26:35 -0300

    bias test

--- examples/example_sphere_proj.py
+++ examples/example_sphere_proj.py
@@ -171,6 +171,8 @@ def compute_spherical_projection():
         cov = np.asanyarray(m)
         std_ = np.sqrt(np.diag(cov))
         corr = cov / np.outer(std_, std_)
+        print(len(cov))
+        print(len(cov[0]))
         plt.matshow(
             cov,
             extent=np.array([z_a[0], z_a[-1], z_a[0], z_a[-1]]),
@@ -194,80 +196,7 @@ def compute_spherical_projection():
     # plt.xlim ([200.0, 4000.0])
 
     # plt.show ()
-    print(cov / np.log(10))
 
-    """""" """
-    import sys
-    sys.path.insert(0, '../../Programas_Cosmologia/')
-    import PySSC
-    from PySSC import PySSC
-    zstakes = np.array([0.2,0.3,0.4,0.5,0.6,0.7,0.8,  ,1,1.1,1.2,1.3,1.4,1.5])
-
-    nz       = 2000
-    z_arr    = np.linspace(0.2,2,num=nz+1)[1:] 
-
-
-
-    nbins_T   = len(zstakes)-1
-    kernels_T = np.zeros((nbins_T,nz))
-    for i in range(nbins_T):
-        zminbin = zstakes[i] ; zmaxbin = zstakes[i+1] ; Dz = zmaxbin-zminbin
-        for iz in range(nz):
-            z = z_arr[iz]
-            if ((z>zminbin) and (z<=zmaxbin)):
-                kernels_T[i,iz] = 1/Dz
-
-
-
-    Sijw_T_alt = PySSC.Sij_alt_fullsky(z_arr,kernels_T)
-    """ """"""
-
-    """""" """""" """""" """""" """""" """""" """""" """""" """""" """
-    # Compute necessary FFTs and make interpolation functions
-    def lacasa_cll(z):
-        nz = len(z)
-        keq         = 0.02/cosmo.h()                            #Equality matter radiation in 1/Mpc (more or less)
-        klogwidth   = 10                                        #Factor of width of the integration range.
-                                                                #10 seems ok ; going higher needs to increase nk_fft to reach convergence (fine cancellation issue noted in Lacasa & Grain)
-        kmin        = 1.0e-6
-        kmax        = 1.0e2
-        nk_fft      = 2**11                                     #seems to be enough. Increase to test precision, reduce to speed up.
-        k_4fft      = np.linspace(kmin,kmax,nk_fft)             #linear grid on k, as we need to use an FFT
-        Deltak      = kmax - kmin
-        Dk          = Deltak/nk_fft
-        Pk_4fft     = np.zeros(nk_fft)
-        for ik in range(nk_fft):
-            Pk_4fft[ik] = ps.eval(cosmo,0.,k_4fft[ik])               #In Mpc^3
-        dr_fft      = np.linspace(0,nk_fft//2,nk_fft//2+1)*2*np.pi/Deltak
-
-        
-        fft0        = np.fft.rfft(Pk_4fft)*Dk
-        dct0        = fft0.real ; dst0 = -fft0.imag
-        Pk_dct      = interp1d(dr_fft,dct0,kind='cubic')
-        
-        # Compute sigma^2(z1,z2)
-        sigma2_nog = np.zeros((nz,nz))
-        #First with P(k,z=0) and z1<=z2
-        for iz in range(nz):
-            r1 =   dist.comoving (cosmo, z[iz])
-            for jz in range(iz,nz):
-                r2                =  dist.comoving (cosmo, z[jz])
-                rsum              = r1+r2
-                rdiff             = abs(r1-r2)
-                Icp0              = Pk_dct(rsum) ; Icm0 = Pk_dct(rdiff)
-                sigma2_nog[iz,jz] = (Icm0-Icp0)/(4*np.pi**2 * r1 * r2)
-        #Now fill by symmetry and put back growth functions
-        sigma2      = np.zeros((nz,nz))
-        for iz in range(nz):
-            growth1 = ps.eval(cosmo,z[iz],0.1)/ps.eval(cosmo,0.,0.1)
-            for jz in range(nz):
-                growth2       = ps.eval(cosmo,z[jz],0.1)/ps.eval(cosmo,0.,0.1)
-                sigma2[iz,jz] = sigma2_nog[min(iz,jz),max(iz,jz)]*growth1*growth2
-
-        return sigma2
-
-    print(lacasa_cll(z_a))
-    """ """""" """""" """""" """""" """""" """""" """""" """""" """"""
 
 
 if __name__ == "__main__":
