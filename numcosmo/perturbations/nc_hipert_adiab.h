8afd59047288a14d6a852752b566b3bcf07c7ef5
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2024-04-22 17:42:48 -0300

    Added spectrum computation to NcHIPertEM and updated notebook.

--- numcosmo/perturbations/nc_hipert_adiab.h
+++ numcosmo/perturbations/nc_hipert_adiab.h
@@ -29,10 +29,9 @@
 #include <glib.h>
 #include <glib-object.h>
 #include <numcosmo/build_cfg.h>
-#include <numcosmo/math/ncm_ode_spline.h>
-#include <numcosmo/math/ncm_powspec_spline2d.h>
 #include <numcosmo/nc_hicosmo.h>
 #include <numcosmo/math/ncm_csq1d.h>
+#include <numcosmo/math/ncm_powspec_spline2d.h>
 
 G_BEGIN_DECLS
 
