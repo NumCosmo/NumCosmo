375b0c682d7ded8ab491167219ea76f58d3224b9
Mariana Penna Lima <pennalima@gmail.com>
2024-12-18 09:29:56 -0300

    Create notebook and minor fix in the documentation.

--- numcosmo/xcor/nc_xcor_limber_kernel_weak_lensing.c
+++ numcosmo/xcor/nc_xcor_limber_kernel_weak_lensing.c
@@ -30,7 +30,7 @@
  *
  * The kernel is given by
  * \begin{equation}
- *    W^{\kappa_{\mathrm{gal}}} = \frac{3}{2} \frac{\Omega_m H_0^2}{c} \frac{(1+z)}{H(z)} \chi(z) \int dz^\prime \frac{dn}{dz^\prime} \frac{\chi(^\prime) - \chi(z)}{\chi(^\prime)}
+ *    W^{\kappa_{\mathrm{gal}}} = \frac{3}{2} \frac{\Omega_m H_0^2}{c} \frac{(1+z)}{H(z)} \chi(z) \int dz^\prime \frac{dn}{dz^\prime} \frac{\chi(z^\prime) - \chi(z)}{\chi(z^\prime)}
  * \end{equation}
  *
  * where $\frac{dn}{dz}$ is the redshift distribution of galaxies.
