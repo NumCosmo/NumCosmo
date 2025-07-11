a74f501f40532714b3dd30c3969339ea34ce075b
Caio Lima de Oliveira <code.caio@limadeoliveira.me>
2025-07-11 15:26:58 -0300

    Refactor galaxy redshift limit functions (#222)
    * Refactor galaxy redshift limit functions
    * Renamed and implemented new functions for obtaining redshift limits in the galaxy redshift objects:
      - Changed `nc_galaxy_sd_obs_redshift_get_lim` to `nc_galaxy_sd_obs_redshift_get_integ_lim` for integration limits.
      - Added `nc_galaxy_sd_obs_redshift_get_zp_lim` for distribution limits.
      - Updated related classes and structures to accommodate the new function signatures and ensure proper functionality.
    * Adding helper functions for gaussian integral computation.
    * Moved local implementation to the object where it is related to.
    * Using correct normalization for z-gauss.
    * Fixing method/variable names.
    * Computing the log of the normalization.
    * Updating tests.
    * Improving tests for gaussian integral.
    * Testing missing lines.
    
    ---------
    
    Co-authored-by: Sandro Dias Pinto Vitenti <vitenti@uel.br>

--- numcosmo/math/ncm_util.c
+++ numcosmo/math/ncm_util.c
@@ -547,6 +547,178 @@ ncm_util_mln_1mIexpzA_1pIexpmzA (const gdouble rho, const gdouble theta, const g
   theta1[0] = cimag (zp);
 }
 
+#define ERF_BOUND (3.0)
+
+/**
+ * ncm_util_normal_gaussian_integral:
+ * @xl: the lower bound
+ * @xu: the upper bound
+ *
+ * Computes the integral of the Gaussian distribution with zero mean and unit variance
+ * between @xl and @xu
+ *
+ * Returns: value of the integral.
+ */
+gdouble
+ncm_util_normal_gaussian_integral (const gdouble xl, const gdouble xu)
+{
+  if (xl == xu)
+    return 0.0;
+
+  {
+    const gdouble sqrt_half = M_SQRT1_2;
+    gdouble ul, uu, sign;
+
+    if (xl < xu)
+    {
+      ul   = xl * sqrt_half;
+      uu   = xu * sqrt_half;
+      sign = +1.0;
+    }
+    else
+    {
+      ul   = xu * sqrt_half;
+      uu   = xl * sqrt_half;
+      sign = -1.0;
+    }
+
+    if (ul > ERF_BOUND)
+    {
+      /*
+       * When both ul and uu are in the right tail (ul > ERF_BOUND), erf(x) ≈ 1 and
+       * loses precision. Use erfc(x) = 1 - erf(x) for better accuracy since x >
+       * ERF_BOUND.
+       */
+      const gdouble val = 0.5 * (erfc (ul) - erfc (uu));
+
+      return sign * val;
+    }
+    else if (uu < -ERF_BOUND)
+    {
+      /*
+       * When both ul and uu are in the left tail (uu < -ERF_BOUND), erf(x) approximates
+       * -1 and is numerically unstable. Use erf(x) = -erf(-x) = erfc(-x) -1 for better
+       * accuracy since -x > ERF_BOUND.
+       */
+      const gdouble val = 0.5 * (erfc (-uu) - erfc (-ul));
+
+      return sign * val;
+    }
+    else
+    {
+      /*
+       * Otherwise, we can use erf.
+       */
+      const gdouble val = 0.5 * (erf (uu) - erf (ul));
+
+      return sign * val;
+    }
+  }
+}
+
+/**
+ * ncm_util_gaussian_integral:
+ * @xl: the lower bound
+ * @xu: the upper bound
+ * @mu: the mean
+ * @sigma: the standard deviation
+ *
+ * Computes the integral of the Gaussian distribution with mean @mu and standard deviation @sigma
+ * between @xl and @xu.
+ *
+ * Returns: value of the integral.
+ */
+gdouble
+ncm_util_gaussian_integral (const gdouble xl, const gdouble xu, const gdouble mu, const gdouble sigma)
+{
+  return ncm_util_normal_gaussian_integral ((xl - mu) / sigma, (xu - mu) / sigma);
+}
+
+/**
+ * ncm_util_log_normal_gaussian_integral:
+ * @xl: the lower bound
+ * @xu: the upper bound
+ * @sign: (nullable): pointer to a gdouble to store the sign of the integral
+ *
+ * Computes the logarithm of the integral of the Gaussian distribution with zero mean
+ * and unit variance between @xl and @xu. If @sign is not %NULL, the sign of the
+ * result (+/-1) is stored in *@sign.
+ *
+ * Returns: log of the absolute value of the integral.
+ */
+gdouble
+ncm_util_log_normal_gaussian_integral (const gdouble xl, const gdouble xu, gdouble *sign)
+{
+  if (xl == xu)
+    return -INFINITY;
+
+  {
+    const gdouble sqrt_half = M_SQRT1_2;
+    gdouble ul, uu;
+
+    if (xl < xu)
+    {
+      ul    = xl * sqrt_half;
+      uu    = xu * sqrt_half;
+      *sign = 1.0;
+    }
+    else
+    {
+      ul    = xu * sqrt_half;
+      uu    = xl * sqrt_half;
+      *sign = -1.0;
+    }
+
+    if (ul > ERF_BOUND)
+    {
+      const gdouble val = 0.5 * (erfc (ul) - erfc (uu));
+
+      return log (fabs (val));
+    }
+    else if (uu < -ERF_BOUND)
+    {
+      const gdouble val = 0.5 * (erfc (-ul) - erfc (-uu));
+
+      return log (fabs (val));
+    }
+    else if ((uu > ERF_BOUND) && (ul < -ERF_BOUND))
+    {
+      const gdouble val = -0.5 * (erfc (uu) + erfc (-ul));
+
+      return log1p (val);
+    }
+    else
+    {
+      const gdouble val = 0.5 * (erf (uu) - erf (ul));
+
+      return log (fabs (val));
+    }
+  }
+}
+
+/**
+ * ncm_util_log_gaussian_integral:
+ * @xl: the lower bound
+ * @xu: the upper bound
+ * @mu: the mean
+ * @sigma: the standard deviation
+ * @sign: (nullable): pointer to a gdouble to store the sign of the integral
+ *
+ * Computes the logarithm of the integral of the Gaussian distribution with mean @mu and
+ * standard deviation @sigma between @xl and @xu. Uses the standard normal log integral.
+ * If @sign is not %NULL, the sign of the result (+/-1) is stored in *@sign.
+ *
+ * Returns: log of the integral value.
+ */
+gdouble
+ncm_util_log_gaussian_integral (const gdouble xl, const gdouble xu, const gdouble mu, const gdouble sigma, gdouble *sign)
+{
+  const gdouble zl = (xl - mu) / sigma;
+  const gdouble zu = (xu - mu) / sigma;
+
+  return ncm_util_log_normal_gaussian_integral (zl, zu, sign);
+}
+
 /**
  * ncm_cmp:
  * @x: a double
