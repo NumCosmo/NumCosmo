1e2c374babb3f4d375c670fe9ac73d50758ac5f8
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2025-07-23 19:13:39 -0300

    Improving stub generation. (#225)
    * Improving stub generation to include numpy arrays.
    * Updating stubs.
    * Removing unnecessary conversions.
    * Fixing typos.
    * Using conda instead of mamba in rtd.

--- numcosmo/perturbations/nc_hipert_two_fluids.c
+++ numcosmo/perturbations/nc_hipert_two_fluids.c
@@ -29,7 +29,7 @@
  * Perturbation object for a two fluids system.
  *
  * This object provides the computation of the two fluid system of cosmological
- * perturbations. This problem is decribed by two fluids with energy density and
+ * perturbations. This problem is described by two fluids with energy density and
  * pressure given respectively by $\bar{\rho}_i$ and $\bar{p}_i$ for $i = 1,2$.
  *
  * The system is written in terms of the gauge invariant variable
@@ -1242,7 +1242,7 @@ nc_hipert_two_fluids_set_init_cond (NcHIPertTwoFluids *ptf, NcHICosmo *cosmo, gd
 
 #ifdef HAVE_SUNDIALS_ARKODE
   ARKRhsFn fE, fI;
-  ARKLsJacFn dfI_dy;
+  ARKLSJacFn dfI_dy;
 #endif /* HAVE_SUNDIALS_ARKODE */
 
   if (vtype != c_vtype)
@@ -1972,7 +1972,7 @@ nc_hipert_two_fluids_get_cross_time (NcHIPertTwoFluids *ptf, NcHICosmo *cosmo, N
     g_warning ("%s", gsl_strerror (status));
 
   if (iter >= max_iter)
-    g_warning ("nc_hipert_two_fluids_get_cross_time: maximum number of interations reached.");
+    g_warning ("nc_hipert_two_fluids_get_cross_time: maximum number of iterations reached.");
 
   gsl_root_fsolver_free (s);
 
