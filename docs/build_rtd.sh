f29f848a1024861df7bcfc24abcda4fec6e9a933
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2025-10-14 13:25:25 -0300

    Testing RTD build.

--- docs/build_rtd.sh
+++ docs/build_rtd.sh
@@ -2,6 +2,7 @@
 
 set -e
 source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate base
+conda env update --name numcosmo_developer --file devel_environment.yml --prune
+conda activate numcosmo_developer
 meson setup build -Ddocumentation=true -Db_lto=false
 meson compile -C build
