908f43cb4c17187324afeabae84413a3692beada
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2025-10-14 13:48:00 -0300

    Testing RTD build.

--- docs/build_rtd.sh
+++ docs/build_rtd.sh
@@ -4,7 +4,7 @@ set -e
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda env update --name numcosmo_developer --file devel_environment.yml --prune
 conda activate numcosmo_developer
-export JUPYTER_RUNTIME_DIR=$(pwd)/.jupyter_runtime
-mkdir -p "$JUPYTER_RUNTIME_DIR"
+wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.6.42/quarto-1.6.42-linux-amd64.tar.gz && mkdir ~/opt && tar -C ~/opt -xvzf quarto-1.6.42-linux-amd64.tar.gz
+export PATH="$HOME/opt/quarto-1.6.42/bin:$PATH"
 meson setup build -Ddocumentation=true -Db_lto=false
 meson compile -C build
