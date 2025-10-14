4da30c0320e357e8c66933cfa750918e9a26816c
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2025-10-14 13:38:20 -0300

    Testing RTD build.

--- docs/build_rtd.sh
+++ docs/build_rtd.sh
@@ -4,5 +4,7 @@ set -e
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda env update --name numcosmo_developer --file devel_environment.yml --prune
 conda activate numcosmo_developer
+export JUPYTER_RUNTIME_DIR=$(pwd)/.jupyter_runtime
+mkdir -p "$JUPYTER_RUNTIME_DIR"
 meson setup build -Ddocumentation=true -Db_lto=false
 meson compile -C build
