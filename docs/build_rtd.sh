88d66e04ad18b4ce918a09dd203bce955da78dc9
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2025-10-14 14:13:24 -0300

    Testing RTD build.

--- docs/build_rtd.sh
+++ docs/build_rtd.sh
@@ -5,5 +5,7 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 conda env update --name numcosmo_developer --file devel_environment.yml --prune
 conda activate numcosmo_developer
 meson setup build -Ddocumentation=true -Db_lto=false
+ls build/docs
 quarto render build/docs --output-dir test-site --execute-debug --log-level debug --no-cache --execute-daemon-restart --debug
+echo "Quarto site built"
 meson compile -C build
