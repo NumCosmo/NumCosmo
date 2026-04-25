f8ee8c8b9d72a2203fa7b98f4a6a57ee933a3d03
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2026-04-25 17:05:42 -0300

    Using existing fixtures.

--- tests/python/powspec/test_powspec.py
+++ tests/python/powspec/test_powspec.py
@@ -24,8 +24,6 @@
 
 """Unit tests for NumCosmo power-spectra without CCL dependency."""
 
-# pylint: disable=redefined-outer-name
-
 import pytest
 
 import numpy as np
@@ -38,37 +36,13 @@ pytestmark = pytest.mark.powspec
 Ncm.cfg_init()
 
 
-@pytest.fixture
-def nc_cosmo() -> Nc.HICosmo:
-    """Create a simple NumCosmo cosmology object."""
-    cosmo = Nc.HICosmoDEXcdm()
-    reion = Nc.HIReionCamb.new()
-    prim = Nc.HIPrimPowerLaw.new()
-
-    cosmo.add_submodel(reion)
-    cosmo.add_submodel(prim)
-
-    # Set default parameters
-    cosmo.props.H0 = 70.0
-    cosmo.props.Omegab = 0.05
-    cosmo.props.Omegac = 0.25
-    cosmo.props.Omegax = 0.70
-    cosmo.props.Tgamma0 = 2.7255
-    cosmo.props.w = -1.0
-
-    prim.props.n_SA = 0.96
-    prim.props.ln10e10ASA = 3.0
-
-    return cosmo
-
-
-def test_powspec_class(nc_cosmo: Nc.HICosmo) -> None:
+def test_powspec_class(cosmo: Nc.HICosmo) -> None:
     """Test the power spectrum class."""
     ps_ml = Nc.PowspecMLCBE.new()
     cbe = ps_ml.peek_cbe()
     cbe.use_ppf(True)
 
-    ps_ml.prepare(nc_cosmo)
+    ps_ml.prepare(cosmo)
 
     ik_min = ps_ml.get_intern_k_min()
     ik_max = ps_ml.get_intern_k_max()
@@ -80,18 +54,18 @@ def test_powspec_class(nc_cosmo: Nc.HICosmo) -> None:
 
     k_a = np.geomspace(kmin, kmax, 100)
 
-    ps_a = np.array([ps_ml.eval(nc_cosmo, 0.0, k) for k in k_a])
+    ps_a = np.array([ps_ml.eval(cosmo, 0.0, k) for k in k_a])
 
     assert np.all(np.isfinite(ps_a))
 
 
-def test_powspec_class_deriv_z(nc_cosmo: Nc.HICosmo) -> None:
+def test_powspec_class_deriv_z(cosmo: Nc.HICosmo) -> None:
     """Test the power spectrum class."""
     ps_ml = Nc.PowspecMLCBE.new()
     cbe = ps_ml.peek_cbe()
     cbe.use_ppf(True)
 
-    ps_ml.prepare(nc_cosmo)
+    ps_ml.prepare(cosmo)
 
     ik_min = ps_ml.get_intern_k_min()
     ik_max = ps_ml.get_intern_k_max()
@@ -108,5 +82,5 @@ def test_powspec_class_deriv_z(nc_cosmo: Nc.HICosmo) -> None:
     diff = Ncm.Diff.new()
 
     for k in k_a:
-        dps, _ = diff.rc_d1_1_to_1(0.2, lambda z, k0: ps_ml.eval(nc_cosmo, z, k0), k)
-        assert_allclose(dps, ps_ml.deriv_z(nc_cosmo, 0.2, k), atol=0.0, rtol=1.0e-10)
+        dps, _ = diff.rc_d1_1_to_1(0.2, lambda z, k0: ps_ml.eval(cosmo, z, k0), k)
+        assert_allclose(dps, ps_ml.deriv_z(cosmo, 0.2, k), atol=0.0, rtol=1.0e-10)
