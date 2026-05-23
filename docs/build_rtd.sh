92bbf35237c09ce2dda29dcebcbe5c101675c549
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2026-05-23 19:11:12 -0300

    Testing new RTD scheme.

--- docs/build_rtd.sh
+++ docs/build_rtd.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
+
+echo "Repo=$READTHEDOCS_GITHUB_REPOSITORY"
+echo "SHA=$READTHEDOCS_GIT_COMMIT_HASH"
+
+echo "Testing GH artifact API access..."
+curl -i \
+  https://api.github.com/repos/NumCosmo/NumCosmo/actions/artifacts \
+  || true
+
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda env update --name numcosmo_developer --file environment.yml --prune
 conda activate numcosmo_developer
