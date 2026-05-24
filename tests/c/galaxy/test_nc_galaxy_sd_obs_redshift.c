d66a05e67adb71307b87612bed3e55b1c00eb406
Caio Lima de Oliveira <caiolimadeoliveira@pm.me>
2026-05-24 19:33:02 +0200

    Fix include path for ncm_integrate header in galaxy redshift test

--- tests/c/galaxy/test_nc_galaxy_sd_obs_redshift.c
+++ tests/c/galaxy/test_nc_galaxy_sd_obs_redshift.c
@@ -30,7 +30,7 @@
 #undef GSL_RANGE_CHECK_OFF
 #endif /* HAVE_CONFIG_H */
 #include <numcosmo/numcosmo.h>
-#include <math/ncm_integrate.h>
+#include <numcosmo/math/ncm_integrate.h>
 
 #include <math.h>
 #include <glib.h>
